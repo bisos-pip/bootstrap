a02af58d34021bfaa6deae02930d917cba8728d3
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2019-01-03 08:20:49 -0800

    vernu++.

--- dev/setup.py
+++ dev/setup.py
@@ -17,13 +17,14 @@ def longDescription():
 
 
 #__version__ = get_version('unisos/icm/__init__.py')
-__version__ = '0.21'
+__version__ = '0.22'
 
 
 requires = [
     'unisos.ucf',
     'unisos.icm',
     'unisos.common',
+    'unisos.utils',    
     'bisos.common',
     'bisos.platform',
     'blee.icmPlayer',
